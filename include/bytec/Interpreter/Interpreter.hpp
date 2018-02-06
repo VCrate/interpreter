b11dfbcd935eec4963bc4d9d804da8516a484d3c
Hazurl <ricoujules69@gmail.com>
2018-02-06 22:31:33 +0100

    Remove useless signature

--- include/bytec/Interpreter/Interpreter.hpp
+++ include/bytec/Interpreter/Interpreter.hpp
@@ -11,7 +11,7 @@ public:
 
     static void run_next_instruction(SandBox& sandbox);
 
-//private:
+private:
 
     static void write_to(SandBox& sandbox, Argument const& arg, ui32 value);
     static ui32 value_of(SandBox& sandbox, Argument const& arg);
@@ -53,42 +53,6 @@ public:
     static void instruction_HLT(SandBox& sandbox, Instruction const& instruction);
     static void instruction_OUT(SandBox& sandbox, Instruction const& instruction);
     static void instruction_DBG(SandBox& sandbox, Instruction const& instruction);
-
-    static void op_add  (SandBox& sandbox, ui32 operand, ui32& target);
-    static void op_sub  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_mod  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_mul  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_mulu (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_div  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_divu (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_mov  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_pop  (SandBox& sandbox, ui32& target); 
-    static void op_push (SandBox& sandbox, ui32 operand); 
-    static void op_jmp  (SandBox& sandbox, ui32 operand); 
-    static void op_jmpe (SandBox& sandbox, ui32 operand); 
-    static void op_jmpne(SandBox& sandbox, ui32 operand); 
-    static void op_jmpg (SandBox& sandbox, ui32 operand); 
-    static void op_jmpge(SandBox& sandbox, ui32 operand); 
-    static void op_and  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_or   (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_xor  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_neg  (SandBox& sandbox, ui32& target); 
-    static void op_shl  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_rtl  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_shr  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_rtr  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_swp  (SandBox& sandbox, ui32& target0, ui32& target1); 
-    static void op_cmp  (SandBox& sandbox, ui32 operand0, ui32 operand1); 
-    static void op_inc  (SandBox& sandbox, ui32& target); 
-    static void op_dec  (SandBox& sandbox, ui32& target); 
-    static void op_new  (SandBox& sandbox, ui32 operand, ui32& target); 
-    static void op_del  (SandBox& sandbox, ui32 operand); 
-    static void op_hlt  (SandBox& sandbox);
-    static void op_call (SandBox& sandbox, ui32 operand); 
-    static void op_ret  (SandBox& sandbox); 
-    static void op_etr  (SandBox& sandbox);
-    static void op_lve  (SandBox& sandbox);
-    static void op_out  (SandBox& sandbox, ui32 operand); 
 
 };
 
